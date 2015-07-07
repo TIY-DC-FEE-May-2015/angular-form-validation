2d9f3803394d502da3cbcc555a3d88218cf792b6
Tajaa Long <tajaalong@Tajaas-MacBook-Pro.local>
2015-07-07 18:03:19 -0400

    styling changes

--- public/index.js
+++ public/index.js
@@ -2,37 +2,40 @@ angular.module("validationApp", [])
 .controller("FormController", function($scope){
 
 	$scope.invalid = {},
+	
 
 	$scope.$watch('name', function(oldProp, newProp) {
   		console.log(oldProp, newProp)
-  			$scope.submitNewForm(newProp)
+  			$scope.checkNewForm(newProp)
 	}, true);
 
 	$scope.$watch('email', function(oldProp, newProp) {
   		console.log(oldProp, newProp)
-  		$scope.submitNewForm(newProp)
+  		$scope.checkNewForm(newProp)
 	}, true);
 
 	$scope.$watch('website', function(oldProp, newProp) {
   		console.log(oldProp, newProp)
-  		$scope.submitNewForm(newProp)
+  		$scope.checkNewForm(newProp)
 	}, true);
 
 	$scope.$watch('message', function(oldProp, newProp) {
   		console.log(oldProp, newProp)
-  		$scope.submitNewForm(newProp)
+  		$scope.checkNewForm(newProp)
 	}, true);
 
-	$scope.submitNewForm = function() {
+	$scope.checkNewForm = function() {
 		$scope.invalid = {
-			name: !($scope.name),
-			email: !($scope.email),
-			website: !($scope.website),
-			message: !($scope.message),
+			name: !($scope.name) || "",
+			email: !($scope.email) || "" ,
+			website: !($scope.website) || "",
+			message: !($scope.message) || "",
 		}
 	
-
 	}
 
+	$scope.submitNewForm = function(){
+		
+	}
 
  })
